bc9dd999c52a4628ae20824297a59c6fa02671b4
statelesshz <hzji210@gmail.com>
2024-09-19 18:02:10 +0800

    first step

---
+++ examples/offline_inference_npu.py
@@ -0,0 +1,26 @@
+from vllm import LLM, SamplingParams
+
+# Sample prompts.
+prompts = [
+    "Hello, my name is",
+    "The president of the United States is",
+    "The capital of France is",
+    "The future of AI is",
+]
+
+# Create a sampling params object.
+sampling_params = SamplingParams(temperature=0.7,
+                                 top_p=0.8,
+                                 repetition_penalty=1.05,
+                                 max_tokens=512)
+
+# Create an LLM.
+llm = LLM(model="Qwen/Qwen2-7B-Instruct", enforce_eager=True)
+# Generate texts from the prompts. The output is a list of RequestOutput objects
+# that contain the prompt, generated text, and other information.
+outputs = llm.generate(prompts, sampling_params)
+# Print the outputs.
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
